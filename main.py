45293bbe2151968d55ae1ca9d4de243e79cb292f
rchaining@gmail.com <rchaining@gmail.com>
2019-08-23 22:09:27 -0400

    await is not an object

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class DiscordClient(discord.Client):
             else:
                 # spell must contain all of the keywords
                 results = self.sql.spellSearchNameContainsAll(m[2:])
-        elif message.contents.startswith('$identify'):
+        elif message.content.startswith('$identify'):
             toID = message.content.strip().replace('$identify ', '')
             if not toID:
                 return
